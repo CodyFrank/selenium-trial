926eea65d519dd8d075ca64e17738e9cc4cc2606
Cody Frank <cody.frank30@gmail.com>
2021-03-23 14:44:56 -0400

    click log in button

--- index.py
+++ index.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.common.keys import Keys
+
 PATH = "/Users/cody/dev/practice/selenium-trial/chromedriver 2"
 
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://selenium-python.readthedocs.io/")
-print(driver.title)
-time.sleep(2)
-driver.quit()
+driver.get("https://arcane-atoll-75350.herokuapp.com/")
+log_in = driver.find_element_by_css_selector("ul.navbar-nav li:nth-child(2) div .nav-link")
+log_in.click()
+
+# time.sleep(2)
+# driver.quit()
