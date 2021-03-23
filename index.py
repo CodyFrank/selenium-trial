62fb7163012a8353ba51416bba36465ea278e508
Cody Frank <cody.frank30@gmail.com>
2021-03-23 15:26:44 -0400

    get email and password fields in wait block to account for possible latency

--- index.py
+++ index.py
@@ -2,6 +2,9 @@ import secrets
 from selenium import webdriver
 import time
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 PATH = "/Users/cody/dev/practice/selenium-trial/chromedriver 2"
 
@@ -12,11 +15,19 @@ password = secrets.password
 driver.get("https://arcane-atoll-75350.herokuapp.com/")
 log_in = driver.find_element_by_css_selector("ul.navbar-nav li:nth-child(2) div .nav-link")
 log_in.click()
-email_field = driver.find_element_by_id("email")
-password_field = driver.find_element_by_id("password")
-email_field.send_keys(email)
-password_field.send_keys(password)
-password_field.send_keys(Keys.RETURN)
 
-# time.sleep(2)
-# driver.quit()
+try:
+    email_field = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "email"))
+    )
+    password_field = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "password"))
+    )
+finally:
+    email_field.send_keys(email)
+    password_field.send_keys(password)
+    password_field.send_keys(Keys.RETURN)
+    time.sleep(5)
+    driver.quit()
+
+
