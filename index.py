70a552e6abe7a00b07d6b0e1d2fa89b111a0bcb2
Cody Frank <cody.frank30@gmail.com>
2021-03-23 14:05:21 -0400

    open webpage sleep for 2  sec then quit

--- index.py
+++ index.py
@@ -1,6 +1,10 @@
 from selenium import webdriver
+import time
 PATH = "/Users/cody/dev/practice/selenium-trial/chromedriver 2"
 
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://selenium-python.readthedocs.io/")
+print(driver.title)
+time.sleep(2)
+driver.quit()
